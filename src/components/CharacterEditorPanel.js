a666cc89fc7e6c46898bf839208795e0819a7e90
Max Carey <mh.carey@pm.me>
2020-05-06 09:49:57 -0500

    Add width and height props to CharacterEditorPanel.

--- src/components/CharacterEditorPanel.js
+++ src/components/CharacterEditorPanel.js
@@ -4,8 +4,8 @@ import { Stage } from 'react-konva';
 /**
  * A panel where users combine character building blocks to create a character.
  */
-const CharacterEditorPanel = (props) => {
-    return <Stage></Stage>;
+const CharacterEditorPanel = ({ width, height }) => {
+    return <Stage width={width} height={height}></Stage>;
 };
 
 export default CharacterEditorPanel;
--- src/components/CharacterEditorPanel.spec.js
+++ src/components/CharacterEditorPanel.spec.js
@@ -13,10 +13,10 @@ Enzyme.configure({ adapter: new Adapter() });
 
 describe('CharacterEditorPanel', function() {
 
-    it('should begin empty', function() {
-        const wrapper = shallow(<CharacterEditorPanel />);
+    it('should display an empty stage with the given dimensions', function() {
+        const wrapper = shallow(<CharacterEditorPanel width="12" height="17"/>);
 
-        expect(wrapper).to.contain(<Stage></Stage>);
+        expect(wrapper).to.contain(<Stage width="12" height="17"></Stage>);
     });
 
 });
