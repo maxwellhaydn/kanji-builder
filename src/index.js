0007a93cbcbfccb86ab153da2c4020b16cfa654b
Max Carey <mh.carey@pm.me>
2020-05-07 09:41:19 -0500

    Add sample index page with placeholder data

---
+++ src/index.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './components/App';
+
+const buildingBlocks = [{
+    id: 1,
+    category: 'Hats',
+    image: '/images/lid.png'
+}, {
+    id: 2,
+    category: 'Hats',
+    image: '/images/roof.png'
+}, {
+    id: 3,
+    category: 'Feet',
+    image: '/images/four_feet.png'
+}, {
+    id: 4,
+    category: 'Blocks',
+    image: '/images/mouth.png'
+}];
+
+const matches = [];
+
+ReactDOM.render(
+    <React.StrictMode>
+        <App
+            buildingBlocks={buildingBlocks}
+            matches={matches}
+            editorWidth="600"
+            editorHeight="600"
+        />
+    </React.StrictMode>,
+    document.getElementById('root')
+);
