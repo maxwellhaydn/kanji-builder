3d73525ed259973b85f956c01b4465c0507dc0c3
Max Carey <mh.carey@pm.me>
2020-05-17 13:31:46 -0500

    Add CharacterBuilder and CharacterDetail routes
    Add routes for the character builder ("/") and the character detail
    page ("/detail/:character").

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 import Navbar from 'react-bootstrap/Navbar';
 
 import CharacterBuilder from './CharacterBuilder';
+import CharacterDetail from './CharacterDetail';
 
 const App = (props) => {
     return (
@@ -9,7 +11,17 @@ const App = (props) => {
             <Navbar bg="dark" variant="dark">
                 <Navbar.Brand>Kanji Builder</Navbar.Brand>
             </Navbar>
-            <CharacterBuilder />
+            <Switch>
+                <Route
+                    path="/detail/:character"
+                    render={({ match }) => (
+                        <CharacterDetail character={match.params.character} />
+                    )}
+                />
+                <Route path="/">
+                    <CharacterBuilder />
+                </Route>
+            </Switch>
         </div>
     );
 };
--- src/components/App.spec.js
+++ src/components/App.spec.js
@@ -1,21 +1,48 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { MemoryRouter } from 'react-router-dom';
+import { mount } from 'enzyme';
 import { expect } from 'chai';
 import Navbar from 'react-bootstrap/Navbar';
 
 import App from './App';
 import CharacterBuilder from './CharacterBuilder';
+import CharacterDetail from './CharacterDetail';
 
 describe('App', function() {
 
-    it('should contain a navbar and character builder', function() {
-        const wrapper = shallow(<App />);
+    describe('character builder view', function() {
 
-        expect(wrapper).to.contain.exactly(1).descendants(Navbar);
-        expect(wrapper.find(Navbar)).to.contain(
-            <Navbar.Brand>Kanji Builder</Navbar.Brand>
-        );
-        expect(wrapper).to.containMatchingElement(<CharacterBuilder />);
+        it('should contain a navbar and character builder', function() {
+            const wrapper = mount(
+                <MemoryRouter initialEntries={['/']}>
+                    <App />
+                </MemoryRouter>
+            );
+
+            expect(wrapper).to.contain.exactly(1).descendants(Navbar);
+            expect(wrapper.find(Navbar)).to.contain(
+                <Navbar.Brand>Kanji Builder</Navbar.Brand>
+            );
+            expect(wrapper).to.containMatchingElement(<CharacterBuilder />);
+        });
+
+    });
+
+    describe('character detail view', function() {
+
+        it('should contain a navbar and character details', function() {
+            const wrapper = mount(
+                <MemoryRouter initialEntries={['/detail/foo']}>
+                    <App />
+                </MemoryRouter>
+            );
+
+            expect(wrapper).to.contain.exactly(1).descendants(Navbar);
+            expect(wrapper).to.containMatchingElement(
+                <CharacterDetail character="foo" />
+            );
+        });
+
     });
 
 });
