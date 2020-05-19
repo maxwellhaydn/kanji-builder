e86b9c177481ab0340bd00a0610d8deb59f09f73
Max Carey <mh.carey@pm.me>
2020-05-19 10:54:50 -0500

    Show building blocks grouped into categories

--- src/components/CharacterBuildingBlockList.js
+++ src/components/CharacterBuildingBlockList.js
@@ -4,6 +4,35 @@ import ToggleButtonGroup from 'react-bootstrap/ToggleButtonGroup';
 
 import buildingBlocks from '../data/building-blocks.json';
 
+// Group building blocks by category
+const grouped = buildingBlocks.reduce((acc, cur) => {
+    const category = cur.category;
+    if (!acc[category]) acc[category] = [];
+    acc[category].push(cur);
+
+    return acc;
+}, {});
+
+// Sort categories naturally
+const collator = new Intl.Collator('en', {
+    numeric: true,
+    sensitivity: 'base'
+});
+const categories = Object.keys(grouped).sort(collator.compare);
+
+// Create an array of categories and building blocks in the order we will
+// display them. This is a workaround because react-bootstrap's
+// ToggleButtonGroup breaks if it contains anything other than buttons, e.g.
+// React.Fragment
+const ordered = [];
+categories.forEach(category => {
+    ordered.push({ name: category, isCategory: true });
+
+    grouped[category].forEach(buildingBlock => {
+        ordered.push(buildingBlock);
+    });
+});
+
 /**
  * A list of character building blocks divided into categories.
  */
@@ -15,17 +44,31 @@ const CharacterBuildingBlockList = ({ onChange, selected }) => {
             value={selected}
             onChange={onChange}
         >
-            {buildingBlocks.map(item => {
-                 return (
-                     <ToggleButton
-                         className="character-building-block"
-                         key={item.literal}
-                         value={item.literal}
-                         variant="light"
-                     >
-                         {item.literal}
-                     </ToggleButton>
-                 );
+            {ordered.map(item => {
+                if (item.isCategory) {
+                    return (
+                        <ToggleButton
+                            className="character-building-block-category"
+                            key={item.name}
+                            variant="primary"
+                            disabled
+                        >
+                            {item.name}
+                        </ToggleButton>
+                    );
+                }
+                else {
+                    return (
+                        <ToggleButton
+                            className="character-building-block"
+                            key={item.literal}
+                            value={item.literal}
+                            variant="light"
+                        >
+                            {item.literal}
+                        </ToggleButton>
+                    );
+                }
             })}
         </ToggleButtonGroup>
     );
