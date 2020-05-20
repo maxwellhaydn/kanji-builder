27c4073d77f7e554217918517c0f3c045b4d637f
Max Carey <mh.carey@pm.me>
2020-05-20 10:03:20 -0500

    Make character detail page responsive
    On small devices, stack the character literal on top of the details,
    otherwise show the character literal to the left.

--- src/components/CharacterDetail.js
+++ src/components/CharacterDetail.js
@@ -1,5 +1,8 @@
 import React from 'react';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
 
+import './CharacterDetail.css';
 import details from '../data/character-details.json';
 
 /**
@@ -9,19 +12,31 @@ import details from '../data/character-details.json';
 const CharacterDetail = ({ character }) => {
     return (
         <div className="character-detail">
-            <div className="character-literal">{character}</div>
-            <h2>Readings</h2>
-            <ul className="character-readings">
-                {details[character].readings.map((reading, index) => {
-                    return <li key={index}>{reading}</li>;
-                })}
-            </ul>
-            <h2>Meanings</h2>
-            <ul className="character-meanings">
-                {details[character].meanings.map((meaning, index) => {
-                    return <li key={index}>{meaning}</li>;
-                })}
-            </ul>
+            <Row>
+                <Col xs={12} sm={4} md={3} className="justify-content-center">
+                    <div className="character-literal">
+                        <svg viewBox="0 0 100 100">
+                            <text x="50%" y="40%" fontSize="90">
+                                {character}
+                            </text>
+                        </svg>
+                    </div>
+                </Col>
+                <Col>
+                    <h2>Readings</h2>
+                    <ul className="character-readings">
+                        {details[character].readings.map((reading, index) => {
+                            return <li key={index}>{reading}</li>;
+                        })}
+                    </ul>
+                    <h2>Meanings</h2>
+                    <ul className="character-meanings">
+                        {details[character].meanings.map((meaning, index) => {
+                            return <li key={index}>{meaning}</li>;
+                        })}
+                    </ul>
+                </Col>
+            </Row>
         </div>
     );
 };
