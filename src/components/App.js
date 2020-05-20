b955f9a081d09e659594d0e3015e32530781c4a9
Max Carey <mh.carey@pm.me>
2020-05-20 10:02:04 -0500

    Wrap main app contents in a Bootstrap container

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
+import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
 
 import CharacterBuilder from './CharacterBuilder';
@@ -12,17 +13,21 @@ const App = (props) => {
             <Navbar bg="dark" variant="dark">
                 <Navbar.Brand>Kanji Builder</Navbar.Brand>
             </Navbar>
-            <Switch>
-                <Route
-                    path="/detail/:character"
-                    render={({ match }) => (
-                        <CharacterDetail character={match.params.character} />
-                    )}
-                />
-                <Route path="/">
-                    <CharacterBuilder />
-                </Route>
-            </Switch>
+            <Container fluid>
+                <Switch>
+                    <Route
+                        path="/detail/:character"
+                        render={({ match }) => (
+                            <CharacterDetail
+                                character={match.params.character}
+                            />
+                        )}
+                    />
+                    <Route path="/">
+                        <CharacterBuilder />
+                    </Route>
+                </Switch>
+            </Container>
             <div className="footer">
                 This site uses the <a href="http://www.edrdg.org/krad/kradinf.html">RADKFILE/KRADFILE</a> and <a href="http://www.edrdg.org/wiki/index.php/KANJIDIC_Project">KANJIDIC</a> dictionary files. These files are the property of the <a href="http://www.edrdg.org/">Electronic Dictionary Research and Development Group</a>, and are used in conformance with the Group's <a href="http://www.edrdg.org/edrdg/licence.html">licence</a>.
             </div>
