58873fd79cac0f77a908a48b943c79210c12b764
Max Carey <mh.carey@pm.me>
2020-05-23 11:06:13 -0500

    Make matching character list carousel responsive

--- src/components/MatchingCharacterList.js
+++ src/components/MatchingCharacterList.js
@@ -6,26 +6,42 @@ import 'slick-carousel/slick/slick-theme.css';
 import './MatchingCharacterList.css';
 import MatchingCharacter from './MatchingCharacter';
 
-const MAX_SLIDES_TO_SHOW = 8;
-
 /**
  * A list of characters that match what was created in the character builder.
  */
 const MatchingCharacterList = ({ matches }) => {
     const numMatches = matches.count();
-    const slidesToShow = numMatches > MAX_SLIDES_TO_SHOW ?
-                                      MAX_SLIDES_TO_SHOW :
-                                      numMatches;
-    const slidesToScroll = slidesToShow;
+
+    // Number of matches to show at a time at each breakpoint
+    const breakpoints = { 992: 8, 768: 5, 576: 3 };
+
+    const responsive = Object.entries(breakpoints).map(([bp, numSlides]) => {
+        const slidesToShow = Math.min(numMatches, numSlides);
+
+        return {
+            breakpoint: parseInt(bp),
+            settings: {
+                slidesToShow: slidesToShow,
+                slidesToScroll: slidesToShow
+            }
+        };
+    });
+
+    // Number of matches to show at a time by default
+    const slidesToShow = Math.min(numMatches, 10);
+
+    const settings = {
+        infinite: true,
+        slidesToShow: slidesToShow,
+        slidesToScroll: slidesToShow,
+        responsive: responsive
+    };
 
     return (
         <div className="matching-character-list text-center">
             <h4 className="num-matches">{numMatches} matches</h4>
-            <Slider
+            <Slider {...settings}
                 className="d-flex align-items-center"
-                infinite={true}
-                slidesToShow={slidesToShow}
-                slidesToScroll={slidesToScroll}
             >
                 {matches.map(character => (
                     <MatchingCharacter key={character} value={character} />
